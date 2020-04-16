6e53136bb55eda55cc3cd55f006e30b74207882d
Chris Forrest <redrider_cr@yahoo.com>
2020-04-16 18:30:22 -0400

    header styled copyright in footer styled

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,21 @@
 import React from "react";
+import styled from 'styled-components'
 
 const Footer = ({ copyright, date }) => {
      return (
           <div className='footer-info'>
-               <p>Copyright: {copyright}</p>
+               <Copyright>Copyright: {copyright}</Copyright>
                <p>Date: {date}</p>
           </div>
      )
 }
+
+const Copyright = styled.p `
+font-family: 'Poppins', sans-serif;
+margin:20px;
+color:red;
+`
+
+
 
 export default Footer
